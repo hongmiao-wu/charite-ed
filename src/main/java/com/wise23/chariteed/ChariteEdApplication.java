3c6cdba999223871b0699f5dba5b440ddaa0f7e6
Jannes <Jannes.Volkens@t-online.de>
2022-11-25 15:30:03 +0100

    Initial QRCodeGenerator commit

--- src/main/java/com/wise23/chariteed/ChariteEdApplication.java
+++ src/main/java/com/wise23/chariteed/ChariteEdApplication.java
@@ -3,11 +3,21 @@ package com.wise23.chariteed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import com.google.zxing.WriterException;
+
 @SpringBootApplication
 public class ChariteEdApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws WriterException, IOException {
+		// Example for creating a QR code
+		String url = "https://www.youtube.de";
+		String fileName = "QR_code.png";
+		int size = 125;
+		String fileType = "png";
+		File qrFile = new File(fileName);
+		QRCodeGenerator.createQRImage(qrFile, url, size, fileType);
 		SpringApplication.run(ChariteEdApplication.class, args);
 	}
-
 }
