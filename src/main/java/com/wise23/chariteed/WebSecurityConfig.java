791c3dd511922867209115947b597f96066980a3
Meo <meo@Meo-MBA.local>
2022-11-27 18:21:08 +0100

    use embedded h2 database

--- src/main/java/com/wise23/chariteed/WebSecurityConfig.java
+++ src/main/java/com/wise23/chariteed/WebSecurityConfig.java
@@ -2,59 +2,63 @@ package com.wise23.chariteed;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.Scanner;
-import java.io.*;
+import javax.sql.DataSource;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
         @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                http.authorizeHttpRequests((requests) -> requests.antMatchers("/", "/home").permitAll().anyRequest()
-                                .authenticated()).formLogin((form) -> form.loginPage("/login").permitAll())
-                                .logout((logout) -> logout.permitAll());
-
-                return http.build();
+        EmbeddedDatabase datasource() {
+                return new EmbeddedDatabaseBuilder()
+                        .setType(EmbeddedDatabaseType.H2)
+                        .setName("test")
+                        .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                        .build();
         }
 
         @Bean
-        public UserDetailsService userDetailsService() {
-                InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-
-                // Read user.csv file
-                String path = System.getProperty("user.dir") + "/users.csv";
-                Scanner file_reader = null;
-
-                try {
-                        file_reader = new Scanner(new File(path));
-                } catch (IOException e) {
-                        e.printStackTrace();
-                }
-
-                // Add users
-                file_reader.useDelimiter("\n");
-                while (file_reader.hasNext()) {
-                        String[] entry = file_reader.next().split(",");
-                        UserDetails user = User.withUsername(entry[0]).password(getPassword(entry[1])).roles(entry[2])
-                                        .build();
-                        userDetailsService.createUser(user);
-                }
-
-                return userDetailsService;
+        JdbcUserDetailsManager users(DataSource dataSource, PasswordEncoder encoder) {
+                UserDetails doctor = User.builder()
+                        .username("meo")
+                        .password(encoder.encode("password"))
+                        .roles("DOCTOR")
+                        .build();
+                JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+                jdbcUserDetailsManager.createUser(doctor);
+                return jdbcUserDetailsManager;
         }
 
-        private String getPassword(String password) {
-                PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-                return encoder.encode(password);
+        @Bean
+        SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                return http
+                        .csrf(csrf -> csrf.ignoringAntMatchers("/h2-console/**"))
+                        .authorizeRequests( auth -> auth
+                                .antMatchers("/h2-console/**").permitAll()
+                                .antMatchers("/", "/home").permitAll()
+                                .anyRequest().authenticated()
+                        )
+                        .headers(headers -> headers.frameOptions().sameOrigin())
+                        .formLogin((form) -> form.loginPage("/login").defaultSuccessUrl("/hello", true).permitAll())
+                        .logout((logout) -> logout.permitAll())
+                        .build();
+        }
+
+        @Bean
+        PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
         }
 }
